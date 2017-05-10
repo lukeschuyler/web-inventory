4cd5a2621b0dd60f7b7f2d4dd73ceaeccbab9081
Luke Schuyler <lukeschuyler1@gmail.com>
2017-05-10 10:58:34 -0500

    added qty to tables

--- src/WasteSession.js
+++ src/WasteSession.js
@@ -7,28 +7,33 @@ class WasteSession extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      wasteSession: {}
+      wasteSession: []
     }
   }
 
   componentDidMount() {
-    axios.get(`https://inventory-manager-ls.herokuapp.com/api/v1/inv_sessions/${this.props.match.params.session}`)
+    axios.get(`https://inventory-manager-ls.herokuapp.com/api/v1/waste_line_items/from/${this.props.match.params.session}`)
     .then(res => {
-      this.setState({ wasteSession: res.data })
-      console.log(res.data)
+      let itemArray = []
+      res.data.forEach(item => {
+        item.product.quantity = item.quantity
+        itemArray.push(item.product)
+      })
+      this.setState({ wasteSession: itemArray })
     })
   }
 
   render() {
-    let { products } = this.state.wasteSession
+    let products = this.state.wasteSession
     return (
       <div className="container">
-        <BootstrapTable data={products} striped={ true } hover={ true } condensed={ true } scrollTop={'Bottom'}>
+        <BootstrapTable data={products} striped={ true } hover={ true } condensed={ true }>
             <TableHeaderColumn isKey dataField='id'>Product ID</TableHeaderColumn>
             <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
             <TableHeaderColumn dataField='upc_code'>UPC Code</TableHeaderColumn>
             <TableHeaderColumn dataField='price'>Price</TableHeaderColumn>
             <TableHeaderColumn dataField='measure'>Measure</TableHeaderColumn>
+            <TableHeaderColumn dataField='quantity'>QTY/Weight</TableHeaderColumn>
         </BootstrapTable>
       </div>
     );
