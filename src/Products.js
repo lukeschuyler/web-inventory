6280d0f7dbed14d621d9370bbfeaf9e579c884f6
Luke Schuyler <lukeschuyler1@gmail.com>
2017-05-10 16:01:08 -0500

    product cards created

--- src/Products.js
+++ src/Products.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class Products extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`https://inventory-manager-ls.herokuapp.com/api/v1/products`)
+    .then(res => {
+      console.log(res.data)
+      this.setState({products: res.data})
+    })
+  }
+
   render() {
     return (
-      <div className="container">
-        <div>
-          <h2>Products</h2>
+      <div className="container product-list-container">
+        <div className="row product-list">
+          {this.state.products.map((p, i) => 
+            <div className="col-xs-4 product-card">
+              <img className="product-image rounded" src={p.image}/>
+              <h4>{p.name}</h4>
+            </div>
+          )}
         </div>
       </div>
     );
