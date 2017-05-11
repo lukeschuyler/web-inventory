2c00944307121d30ca74fa66a372bed31ee4dfa2
Luke Schuyler <lukeschuyler1@gmail.com>
2017-05-11 10:40:15 -0500

    edit btn changing state

--- src/EditProduct.js
+++ src/EditProduct.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import axios from 'axios'
 
 
-const EditProduct = ({ name, price, image, code, description }) =>
+const EditProduct = ({ name, price, image, code, description, done }) =>
    (
       <div className="col-xs-4 product-card">
         <img className="product-image rounded" src={image} />
+        <icon onClick={done} className="btn btn-sm glyphicon edit-btn glyphicon-ok"></icon>
         <input type="text" value={name} />
         <span><input type="text" value={price} /></span>
         <span>{code}</span>
-        <textarea value={description} className="product-description"></textarea>
+        <textarea value={description} className="product-description-edit"></textarea>
       </div>
     );
 
--- src/Product.js
+++ src/Product.js
@@ -6,26 +6,41 @@ class Product extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      p: this.props.product
+      p: this.props.product,
+      editing: false
     }
+    this.edit = this.edit.bind(this)
   }
 
   edit() {
-
+    this.setState({editing: !this.state.editing})
   }
 
   render() {
    let p = this.state.p
+   if(!this.state.editing) {
     return (
       <div className="col-xs-4 product-card">
         <img className="product-image rounded" src={p.image}/>
-        <icon onClick={this.edit} className="btn btn-sm glyphicon edit-btn glyphicon-pencil"></icon>
+        <icon onClick={this.edit} className="btn glyphicon edit-btn glyphicon-edit"></icon>
         <h4>{p.name}</h4>
         <span>${p.price}</span><br />
         <span>{p.upc_code}</span>
         <div className="product-description"><p>{p.description}</p></div>
       </div>
-    );
+    );   
+   } else { 
+    return (
+      <EditProduct 
+        name={p.name}
+        price={p.price}
+        image={p.image}
+        code={p.upc_code}
+        description={p.description}
+        done={this.edit}
+      />
+    )
+   }
   }
 }
 
