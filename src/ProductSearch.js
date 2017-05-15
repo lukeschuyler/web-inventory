e46a7b4504017cee1f6c230456c892f879667459
Luke Schuyler <lukeschuyler1@gmail.com>
2017-05-15 18:27:23 -0500

    working catch

--- src/ProductSearch.js
+++ src/ProductSearch.js
@@ -9,34 +9,39 @@ class ProductSearch extends Component {
     this.state = {
       query: '',
       products: [],
-      loading: false
+      loading: false,
+      notFound: false
     }
   }
 
   search(e) {
     this.setState({loading: true})
     e.preventDefault()
-    // axios.post(`http://localhost:8082/api/v1/search`, { query: this.state.query })
     axios.post(`https://inventory-manager-ls.herokuapp.com/api/v1/search`, { query: this.state.query })
     .then(res => {
       console.log(res.data)
-      this.setState({ products: res.data, loading: false })
+      this.setState({ products: res.data, loading: false, notFound: false })
+    })
+    .catch(err => {
+      this.setState({notFound:true})
     })
   }
 
   render() {
     let loading = this.state.loading
+    let notFound = this.state.notFound ? <h3>Item Not Found</h3> : ''
     return (
       <div>
         <div className="container">
           <form>
-            <input value={this.state.query} onChange={(e) => { this.setState({query: e.target.value}) }  }  placeholder="Search For New Products!" className="form-control"/>
+            <input value={this.state.query} onChange={(e) => { this.setState({query: e.target.value}) }  }  placeholder="Search For New Products!" className="amazon-search form-control"/>
             <button onClick={(e) => { this.search(e) } } className="btn btn-primary">Search</button>
           </form>
         </div>
         <div className="container product-list-container">
           <h1>Search Products</h1>
           <hr />
+          {notFound}
           <div className="row">
             {this.state.products.map((p, i) => 
               <ProductCardSearch 
