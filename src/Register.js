8678dc14afbde16ee69701b138b94a0d1819f180
Luke Schuyler <lukeschuyler1@gmail.com>
2017-05-24 12:08:42 -0700

    added Link to Login, made routes for login and register

--- src/Register.js
+++ src/Register.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Link } from 'react';
 
 class Register extends Component {
   constructor(props) {
@@ -9,6 +9,10 @@ class Register extends Component {
     }
   }
 
+  register() {
+    console.log('hey')
+  }
+
   render() {
     return (
       <div>
@@ -17,7 +21,7 @@ class Register extends Component {
             <h1 className="logo">Simply Managed.</h1>
           </div>
         </div>
-        <form className="container">
+        <form onSubmit={this.register} className="container">
           <div className="form-group row">
             <label for="example-text-input" className="col-xs-offset-4 col-xs-1 col-form-label">Email</label>
             <div className="col-xs-3">
@@ -36,6 +40,8 @@ class Register extends Component {
               <input onChange={(e) => { this.setState({passCheck: e.target.value}) }} className="form-control" type="password" value={this.state.passCheck} id="example-search-input" />
             </div>
           </div>
+        <input className="btn btn-success" type="submit" value="Register"/>
+        <Link to="/login" >Back to Login</Link>
       </form>
     </div>
     );
